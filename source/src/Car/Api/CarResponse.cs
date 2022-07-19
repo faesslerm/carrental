434d80511098b2b5d83fd07fd10e4dcb68a80b1b
faesslerm <markus.mfs@gmail.com>
2022-07-19 18:11:40 +0200

    Added class CarResponse

---
+++ source/src/Car/Api/CarResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CarRent.Car.Api
+{
+    public class CarResponse
+    {
+        [JsonPropertyOrder(100)]
+        public Guid Id { get; set; }
+
+        [JsonPropertyOrder(200)]
+        [JsonPropertyName("car-number")]
+        public string CarNumber { get; set; }
+
+        [JsonPropertyOrder(300)]
+        public string Brand { get; set; }
+
+        [JsonPropertyOrder(400)]
+        public string CarClass { get; set; }
+
+        [JsonPropertyOrder(500)]
+        public string Type { get; set; }
+    }
+}
