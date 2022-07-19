55a07d291aab897241c65e0de556d5a08e7ccca1
faesslerm <markus.mfs@gmail.com>
2022-07-19 18:14:53 +0200

    Added interface of car repository

---
+++ source/src/Car/Domain/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRent.Car.Domain
+{
+    public interface ICarRepository
+    {
+        Car GetById(Guid id);
+
+        Car GetByCarNumber(string carNumber);
+
+        // ..
+
+        void Add(Car car);
+
+        void Update(Car car);
+
+        void Remove(Car car);
+    }
+}
