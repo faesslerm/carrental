ac3d5f8c07a2511d989f31fbafd46bca01076b1d
faesslerm <markus.mfs@gmail.com>
2022-07-19 18:04:10 +0200

    Added domain class of car

---
+++ source/src/Car/Domain/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace CarRent.Car.Domain
+{
+    public class Car
+    {
+        public Guid Id { get; }
+
+        public string CarNumber { get; }
+
+        public CarClass CarClass { get; }
+
+        public Brand Brand { get; }
+
+        public Type Type { get; }
+
+    }
+}
