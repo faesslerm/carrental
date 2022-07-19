75620fbb8f735dcef492e8b50794eeb69d34be0c
faesslerm <markus.mfs@gmail.com>
2022-07-19 18:12:34 +0200

    Added CarController

---
+++ source/src/Car/Api/CarController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarRent.Car.Api
+{
+    [Route("api/car")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+
+        // GET: api/<CarController>
+        [HttpGet]
+        public IEnumerable<CarResponse> Get()
+        {
+            return new CarResponse[]
+            {
+                new CarResponse()
+                {
+                    Id = Guid.NewGuid(),
+                    CarNumber = "XW82",
+                    Brand = "Audi",
+                    Type = "SUV",
+                    CarClass = "Luxury"
+                },
+            };
+        }
+
+        // GET api/<CarController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CarController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CarController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CarController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
