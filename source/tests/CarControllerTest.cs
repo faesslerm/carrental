6b7b166d2b2c4875079b9b5ff1844bd004ab820c
faesslerm <markus.mfs@gmail.com>
2022-08-16 18:26:59 +0200

    Test code coverage

---
+++ source/tests/CarControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using CarRent.Car.Api;
+
+namespace CarRent.Tests
+{
+    public class CarControllerTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            var controller = new CarController();
+            Assert.Equal("value", controller.Get(1));
+        }
+    }
+}
