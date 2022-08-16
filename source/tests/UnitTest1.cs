0b0129907c05672b8e21b5b3613d65b63608d790
faesslerm <markus.mfs@gmail.com>
2022-08-16 18:01:56 +0200

    Added sample test

--- source/tests/UnitTest1.cs
+++ source/tests/UnitTest1.cs
@@ -3,8 +3,14 @@ namespace CarRent.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void PassingTest()
     {
+        Assert.Equal(4, 2 + 2);
+    }
 
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.Equal(5, 2 + 2);
     }
 }
