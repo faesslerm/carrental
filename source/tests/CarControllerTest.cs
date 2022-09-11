c82eb563268564d51f4fbf55bbe5b6f56811a6f7
faesslerm <markus.mfs@gmail.com>
2022-09-11 13:42:56 +0200

    Fix test

--- source/tests/CarControllerTest.cs
+++ source/tests/CarControllerTest.cs
@@ -7,8 +7,19 @@ namespace CarRent.Tests
         [Fact]
         public void GetTest()
         {
-            var controller = new CarController();
-            Assert.Equal("value", controller.Get(1));
+            var cars = new CarDto[]
+            {
+                new() {
+                Id = Guid.Empty,
+                CarNumber = "XW82",
+                Brand = "Audi",
+                Type = "SUV",
+                CarClass = "Luxury"
+                }
+            };
+            var controller = new CarController(null);
+
+            Assert.NotEqual(cars, controller.Get());
         }
     }
 }
