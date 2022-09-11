4daf7125acf43c21e035764ec9fb35c685b45bad
faesslerm <markus.mfs@gmail.com>
2022-09-11 08:23:41 +0200

    Added common repository interface

---
+++ source/src/Common/Domain/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRent.Common.Domain
+{
+    public interface IRepository<TAggregate>
+        where TAggregate : Entity, IAggregateRoot
+    {
+        TAggregate GetById(Guid id);
+        void Add(TAggregate car);
+
+        void Update(TAggregate car);
+
+        void Remove(TAggregate car);
+    }
+}
